7c5a10b5faa8e3bcfb958f78798ac289339920ea
SaulNolasco <sauln559@gmail.com>
2025-08-21 10:21:52 -0600

    CrecimientoPoblacion

--- FuncionesPredefinidas/LargoEscalera.py
+++ FuncionesPredefinidas/LargoEscalera.py
@@ -2,7 +2,7 @@
     Escribe un programa que determine el largo que debe tener una escalera, la cual se necesita para alcanzar una altura determinada, cuando se pone contra una casa. La altura que se desea alcanzar y el ángulo que deberá hacer la escalera contra la pared, son datos que te proporciona el usuario. Para calcular el largo de la escalera usa la siguiente ecuación:
 
     largo=altura/coseno(ángulo)
-
+    
     Recuerda que las funciones de Python que calculan seno y coseno, reciben los ángulos en radianes, pero tu programa va a recibir los ángulos en grados.
 
     Entrada
